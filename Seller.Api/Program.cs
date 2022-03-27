520f66dc5f4709d40391bef227b4534dd9091e04
Karads1982 <shinoj.jos@gmail.com>
2022-03-27 13:46:47 +0100

    Program file updated with dependency registrations

--- Seller.Api/Program.cs
+++ Seller.Api/Program.cs
@@ -1,13 +1,26 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
+using Seller.API.Data;
+using Seller.API.Models;
+using Seller.API.Queue;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
-
+builder.Services.Configure<SellerDatabaseSettings>(
+    builder.Configuration.GetSection("SellerDatabaseSettings"));
+builder.Services.Configure<AzureServiceBusSettings>(
+    builder.Configuration.GetSection("AzureServiceBusSettings"));
+builder.Services.AddScoped<ISellerRepository, SellerRepository>();
+builder.Services.AddScoped<IBidRepository, BidRepository>();
+builder.Services.AddScoped<IMessageQueueClient, AzureMessageClient>();
+builder.Services.AddCors(
+    options => options.AddPolicy("corspolicy",
+    builder => { builder.AllowAnyOrigin(); builder.AllowAnyHeader(); builder.AllowAnyMethod(); }
+    ));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,13 +28,10 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
+app.UseCors("corspolicy");
 app.UseAuthentication();
 app.UseAuthorization();
 
